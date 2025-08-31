69f6b6a9230e66765efbd641208535c684407e61
Nursat Makhsot <makhsotnursat@gmail.com>
2025-08-31 18:56:34 +0500

    feat: init фронтенд на angular 20

---
+++ Frontend/coffee-shop/src/app/app.ts
@@ -0,0 +1,10 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App {
+}
